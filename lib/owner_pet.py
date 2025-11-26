ee0e5eb5ca7d31a8d199bc7b2c3544271525641e
Rodney Swaji <rodney.swaji@student.moringaschool.com>
2025-11-26 17:41:22 +0300

    initial commit

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,37 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception("Invalid pet type")
+
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+
+        Pet.all.append(self)
+
+    def __repr__(self):
+        return f"<Pet {self.name} ({self.pet_type})>"
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        """Return all pets whose owner is this Owner"""
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        """Add a pet to this owner after validating type"""
+        if not isinstance(pet, Pet):
+            raise Exception("Must add Pet instance")
+
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        """Return this owner's pets sorted alphabetically by name"""
+        return sorted(self.pets(), key=lambda pet: pet.name)
+
